bf12336621110e4294d36ffd9c8fb1dc40d2c797
Eric Gomez <eric_gomez_1@hotmail.com>
2023-11-18 19:40:30 -0600

    impl. carousel, other small tweaks to ui

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,36 +1,41 @@
-<div class="container mx-auto">
-  <div class="navbar bg-base-100 p-2 mb-4">
-    <div class="">
-      <a class="btn btn-ghost w-full normal-case text-xl whitespace-nowrap" href="/">Mobile Greaser</a>
-    </div>
-    <div class="navbar-start">
-      <a class="link link-hover  p-2" aria-current="page" href="/">Home</a>
-    </div>
-    <div class="navbar-center">
-    </div>
-    <div class="navbar-end">
-    </div>
+<div class="bg-base-200">
+  <div class="container mx-auto ">
+    <nav class="navbar  p-2 mb-4">
+      <div>
+        <a class="btn btn-ghost w-full normal-case text-xl whitespace-nowrap" href="/">Mobile Greaser</a>
+      </div>
+      <div class="navbar-start">
+        <a class="link link-hover  p-2" aria-current="page" href="/">Home</a>
+      </div>
+      <div class="navbar-center">
+      </div>
+      <div class="navbar-end">
+      </div>
+    </nav>
   </div>
 </div>
 
 
+
 <slot/>
 
-<div class="container mx-auto">
-  <footer class="py-3 flex flex-col md:flex-row  md:justify-around">
-    <section class="p-2 flex flex-col lg:justify-between">
-      <h5 class="text-2xl font-bold tracking-tight mb-2">Mobile Greaser</h5>
-      <p class="text-secondary">Copyright © 2023 Mobile Greaser, Inc. All rights reserved</p>
-    </section>
-    <section class="p-2 d-flex flex-col md:justify-between">
-      <h5 class="font-bold mb-2">Company</h5>
-      <p><a class="link link-hover link-primary" href="https://twitter.com">Twitter</a></p>
-      <p><a class="link link-hover link-primary" href="mailto:team@mobilegreaser.com">team@mobilegreaser.com</a></p>
-    </section>
-    <section class="p-2 d-flex flex-column justify-content-lg-between">
-      <h5 class="font-bold mb-2">Legal</h5>
-      <p><a class="link link-hover link-primary" href="https://google.com">Terms of Service</a></p>
-      <p><a class="link link-hover link-primary" href="https://google.com">Privacy Policy</a></p>
-    </section>
-  </footer>
+<div class="bg-base-200">
+  <div class="container mx-auto border">
+    <footer class="py-3 flex flex-col md:flex-row  md:justify-around">
+      <section class="p-2 flex flex-col lg:justify-between">
+        <h5 class="md:text-2xl sm:text-xl font-bold tracking-tight mb-2">Mobile Greaser</h5>
+        <p class="text-secondary">Copyright © 2023 Mobile Greaser, Inc. All rights reserved</p>
+      </section>
+      <section class="p-2 d-flex flex-col md:justify-between">
+        <h5 class="font-bold mb-2">Company</h5>
+        <p><a class="link link-hover link-primary" href="https://twitter.com">Twitter</a></p>
+        <p><a class="link link-hover link-primary" href="mailto:team@mobilegreaser.com">team@mobilegreaser.com</a></p>
+      </section>
+      <section class="p-2 d-flex flex-column justify-content-lg-between">
+        <h5 class="font-bold mb-2">Legal</h5>
+        <p><a class="link link-hover link-primary" href="https://google.com">Terms of Service</a></p>
+        <p><a class="link link-hover link-primary" href="https://google.com">Privacy Policy</a></p>
+      </section>
+    </footer>
+  </div>
 </div>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -7,7 +7,7 @@
 <div class="container mx-auto">
   <div class="flex flex-row">
     <header class="p-3 flex flex-col w-1/2">
-      <h1 class="lg:text-4xl sm:text-2xl font-bold tracking-tight text-center">On Demand Mobile Mechanics</h1>
+      <h1 class="lg:text-4xl sm:text-2xl font-bold tracking-tight text-center mb-3">On Demand Mobile Mechanics</h1>
       <p class="text-secondary text-center">Mobile Greaser helps you service your vehicle any time, any location, at your convenience.</p>
       <div class="my-auto flex flex-row justify-center align-middle items-center">
         <a href="https://play.google.com/store/apps?gl=us" class="w-1/2 mw-200"><img class="mx-auto w-full" src={appleBadge} alt="apple marketplace badge"></a>
@@ -22,12 +22,51 @@
   <div class="divider"></div>
 
   <div class="flex flex-col">
-    <div class="p-3 flex flex-col justify-center items-center">
-      <h2 class="text-2xl font-bold tracking-tight text-center">Save time at the shop</h2>
+    <div class="p-3 flex flex-col justify-center items-center mb-4">
+      <h2 class="sm:text-2xl md:text-3xl font-bold tracking-tight text-center mb-3">Save time at the shop</h2>
       <p class="text-secondary text-center">Join our clients to save time and money on your vehicle maintenance</p>
     </div>
-    <div class="p-3 flex">
-      <img class="mx-auto w-full h-auto" src="https://via.placeholder.com/1200x300" alt="placeholder"/>
+    <div class="w-full h-60 carousel rounded-box items-center py-2">
+      <div class="card card-bordered carousel-item bg-blue-200 bg-opacity-10 shadow-xl max-w-2xl mr-3">
+        <div class="card-body">
+          <h3 class="card-title text-xl text-center">
+            Lorem Ipsum
+          </h3>
+          <p class="overflow-ellipsis">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+      <div class="card card-bordered carousel-item bg-blue-200 bg-opacity-10 shadow-xl max-w-2xl mr-3">
+        <div class="card-body">
+          <h3 class="card-title text-xl text-center">
+            Lorem Ipsum
+          </h3>
+          <p class="overflow-ellipsis">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+      <div class="card card-bordered carousel-item bg-blue-200 bg-opacity-10 shadow-xl max-w-2xl mr-3">
+        <div class="card-body">
+          <h3 class="card-title text-xl text-center">
+            Lorem Ipsum
+          </h3>
+          <p class="overflow-ellipsis">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+      <div class="card card-bordered carousel-item bg-blue-200 bg-opacity-10 shadow-xl max-w-2xl mr-3">
+        <div class="card-body">
+          <h3 class="card-title text-xl text-center">
+            Lorem Ipsum
+          </h3>
+          <p class="overflow-ellipsis">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -35,7 +74,7 @@
 
   <div class="flex flex-row-reverse">
     <div class="p-3 flex flex-col justify-center w-1/2">
-      <h2 class="text-2xl font-bold tracking-tight text-center">Get a complete service report</h2>
+      <h2 class="sm:text-2xl md:text-3xl font-bold tracking-tight text-center mb-3">Get a complete service report</h2>
       <p class="text-secondary text-center">With every service request, we will include a free 50 point inspection report.</p>
     </div>
     <div class="p-3 flex align-middle items-center w-1/2">
@@ -47,7 +86,7 @@
 
   <div class="flex flex-row">
     <div class="p-3 flex flex-col justify-center w-1/2">
-      <h2 class="text-2xl font-bold tracking-tight text-center">Drive with piece of mind</h2>
+      <h2 class="sm:text-2xl md:text-3xl font-bold tracking-tight text-center mb-3">Drive with piece of mind</h2>
       <p class="text-secondary text-center">With our 5000 mile warranty on all services we offer, you never have to worry about improper installation or faulty parts again</p>
     </div>
     <div class="p-3 flex align-middle items-center w-1/2">
@@ -59,7 +98,7 @@
 
   <div class="flex flex-row-reverse">
     <div class="p-3 flex flex-col justify-center w-1/2">
-      <h2 class="text-2xl font-bold tracking-tight text-center">Simple and affordable Pricing</h2>
+      <h2 class="sm:text-2xl md:text-3xl font-bold tracking-tight text-center mb-3">Simple and affordable Pricing</h2>
       <p class="text-secondary text-center">With our flat rate pricing you never have to worry about being up-sold at the shop again.</p>
       <div class="my-auto flex flex-row justify-center align-middle items-center">
         <a href="https://play.google.com/store/apps?gl=us" class="w-1/2 mw-200"><img class="mx-auto w-full" src={appleBadge} alt="apple marketplace badge"></a>
@@ -70,10 +109,6 @@
       <img class="mx-auto" src="https://via.placeholder.com/300" alt="placeholder"/>
     </div>
   </div>
-
-  <div class="divider"></div>
-
-
 </div>
 
 

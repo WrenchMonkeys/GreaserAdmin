44903eb20d127640228ba5be38047b57c98dc0a8
Eric <eric_gomez_1@hotmail.com>
2024-12-18 14:35:20 -0600

    fix mobile aspect ratio

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -124,7 +124,7 @@
 <!--  </div>-->
   <div class="divider"></div>
 
-  <!--ARCADE EMBED START--><div style="position: relative; aspect-ratio: 130.543/100; height: 100%; max-height: 1200px; width: 100%;"><iframe src="https://demo.arcade.software/jMMhViE0XyIkIjzQaLdn?embed&embed_mobile=inline&embed_desktop=inline&show_copy_link=true" title="Mobile Greaser Consumer App" frameborder="0" loading="lazy" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="clipboard-write" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; max-height: 80vh; color-scheme: light;" ></iframe></div><!--ARCADE EMBED END-->
+  <!--ARCADE EMBED START--><div class="aspect-[70/100] md:aspect-[130.543/100]" style="position: relative; height: 100%; max-height: 1200px; width: 100%;"><iframe src="https://demo.arcade.software/jMMhViE0XyIkIjzQaLdn?embed&embed_mobile=inline&embed_desktop=inline&show_copy_link=true" title="Mobile Greaser Consumer App" frameborder="0" loading="lazy" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="clipboard-write" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; max-height: 80vh; color-scheme: light;" ></iframe></div><!--ARCADE EMBED END-->
 
 
   <div class="divider"></div>
